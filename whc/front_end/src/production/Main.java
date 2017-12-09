21a596d2734808daa66e655234785e906de727ce
Prince1995 <amir.romdhane95@gmail.com>
2017-12-09 15:50:36 +0100

    v1_table_sym

--- whc/front_end/src/production/Main.java
+++ whc/front_end/src/production/Main.java
@@ -1,9 +1,17 @@
 package production;
 
+import table_des_symboles.Table;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		Table table_globale = new Table();
+		table_globale.add_function("f", 2, 3);
+		int res[] = table_globale.get_function("f");
+		for (int i = 0; i < res.length; i++)
+			System.out.println(res[i]);
 
 	}
 
--- whc/front_end/src/table_des_symboles/Table.java
+++ whc/front_end/src/table_des_symboles/Table.java
@@ -1,5 +1,23 @@
 package table_des_symboles;
 
-public class Table {
+import java.util.*;
 
+public class Table {
+	//map de fonctions (nom -> paramtre,return, key_tablesymbole_locale, key_table_code3adresse)
+	private HashMap<String, int[]> table = new HashMap<String, int[]>();
+
+	public void add_function(String name, int nb_in, int nb_out) {
+		//les deux dernier paramtres seront traiter dans la fonction
+		
+		int tab[] = new int [4];
+		tab[0]= nb_in;
+		tab[1]= nb_out;
+		
+		table.put(name, tab);
+	}
+	
+	public int[] get_function(String name) {
+		return table.get(name);
+		
+	}
 }
