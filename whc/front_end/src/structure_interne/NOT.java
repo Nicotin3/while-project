1cb8484694f4e772b00dbf91139d8351d812d039
Prince1995 <amir.romdhane95@gmail.com>
2018-01-24 12:22:40 +0100

    expressin and

---
+++ whc/front_end/src/structure_interne/NOT.java
@@ -0,0 +1,18 @@
+package structure_interne;
+
+public class NOT extends Op {
+
+	public NOT() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return "not";
+	}
+	
+	@Override
+	public String getOpName() {
+		return "NOT";
+	}
+}
