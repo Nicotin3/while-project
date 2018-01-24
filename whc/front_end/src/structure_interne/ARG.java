d81cf3245a16c7befe05cf92d2c8d113b9861073
Nicotin3 <32492209+Nicotin3@users.noreply.github.com>
2018-01-24 12:18:42 +0100

    Beta V1.0.2
    Avancement appel de fonction

---
+++ whc/front_end/src/structure_interne/ARG.java
@@ -0,0 +1,18 @@
+package structure_interne;
+
+public class ARG extends Op {
+
+	public ARG() {
+	}
+
+	@Override
+	public String toString() {
+		return "ARG";
+	}
+	
+	@Override
+	public String getOpName() {
+		return "ARG";
+	}
+
+}
