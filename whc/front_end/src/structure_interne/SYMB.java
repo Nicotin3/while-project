e74f781a056574cb44e7783b0f9f49cfc238ca49
ErwanBouet <32834356+ErwanBouet@users.noreply.github.com>
2018-01-25 17:53:11 +0100

    Add files via upload
    petite correction

--- whc/front_end/src/structure_interne/SYMB.java
+++ whc/front_end/src/structure_interne/SYMB.java
@@ -14,6 +14,16 @@ public class SYMB extends Op {
 		return numSymb;
 	}
 	
+	public String getCorrespondingLuaSymb() { // retourne noms de symboles predefinis pour lua
+		switch (numSymb) {
+			case 0 :
+				return "nil";
+		
+			default : 
+				return String.valueOf(numSymb);
+		}
+	}
+	
 	@Override
 	public String toString() {
 		return "SYMB " + numSymb;
