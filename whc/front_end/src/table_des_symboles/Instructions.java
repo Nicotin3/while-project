aab8cbc50bf2730bdc0a10c5194733f383a54189
ErwanBouet <32834356+ErwanBouet@users.noreply.github.com>
2018-01-26 12:18:12 +0100

    Add files via upload
    FOR et WHILE et IF ok

--- whc/front_end/src/table_des_symboles/Instructions.java
+++ whc/front_end/src/table_des_symboles/Instructions.java
@@ -1,9 +1,11 @@
 package table_des_symboles;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 
+import structure_interne.FOR;
 import structure_interne.IF;
 import structure_interne.Op;
 import structure_interne.Quadruplet;
@@ -140,32 +142,42 @@ public class Instructions {
 				break;
 			
 			case "IF":
-				s.append(tab).append("if ");
-				s.append(((IF) (quad.getElement1())).getCondition().toLuaCache(0));
+				tmp = new StringBuilder();
+				tmp.append(tab).append("if ");
 				
-				s.append(tab).append("then\n");
+				if (((IF) (quad.getElement1())).getCondition() == null) {
+					tmp.append(quad.getElement3());
+				}
+				else {
+					s.append(((IF) (quad.getElement1())).getCondition().toLuaCache(indent));
+					tmp.append("var" + quad.getElement3());
+				}
+
+				tmp.append(tab).append("then\n");
+				tmp.append(((IF) quad.getElement1()).getThen().toLuaCache(indent+1));
 				
-				s.append(((IF) quad.getElement1()).getThen().toLuaCache(indent+1));
-				
-				if(((IF) quad.getElement1()).getElse()!=null) {
+				if(((IF) quad.getElement1()).getElse() != null) {
 					s.append(tab).append("else\n");
 					s.append(((IF) quad.getElement1()).getElse().toLuaCache(indent+1));					
 				}
 								
-				s.append(tab).append("end\n");
+				tmp.append(tab).append("end\n");
+				s.append(tmp);
 				break;
 				
 			case "WHILE":
 				tmp = new StringBuilder();
 				tmp.append(tab).append("while ");
 				
-				if (((WHILE) (quad.getElement1())).getCondition().equals(null)) {
-					tmp.append(((WHILE) (quad.getElement1())).getCondition().toLuaCache(0));
+				if (((WHILE) (quad.getElement1())).getCondition() == null) {
+					tmp.append(quad.getElement3());
+				}
+				else {
+					s.append(((WHILE) (quad.getElement1())).getCondition().toLuaCache(indent));
+					tmp.append("var" + quad.getElement3());
 				}
 				
-				
 				tmp.append(tab).append("do\n");
-				
 				tmp.append(((WHILE) (quad.getElement1())).getBoucle().toLuaCache(indent+1));
 				tmp.append(tab).append("end\n");
 				
@@ -173,13 +185,26 @@ public class Instructions {
 				break;
 				
 			case "FOR":
-				s.append(tab).append("for (pas fini)");
+				tmp = new StringBuilder();
+				tmp.append(tab).append("for ");
+				
+				// transformatin de l'expression en con (entier)
+				if (((FOR) (quad.getElement1())).getCondition() != null) {
+					s.append(((FOR) (quad.getElement1())).getCondition().toLuaCache(indent)); 
+				}
+				
+				// ecriture du for
+				tmp.append("i = 0, ");
+				tmp.append("treelib.toNumber("+ "var" + quad.getElement3() + ")" );
+				tmp.append(" do\n");
+				tmp.append(((FOR) (quad.getElement1())).getBoucle().toLuaCache(indent+1));
+				tmp.append(tab).append("od\n");
+				s.append(tmp);
 				break;
 			
 			case "FOREACH":
 				s.append(tab).append("foreach (pas fini)");
 				break;
-			//a completer ici
 				
 			case "AND":
 				elem2=quad.getElement2();
@@ -188,6 +213,7 @@ public class Instructions {
 				s.append("var").append(elem2).append("= var");
 				s.append(elem3).append(" and var").append(elem4);
 				break;
+				
 			case "OR":
 				elem2=quad.getElement2();
 				elem3=quad.getElement3();
@@ -195,12 +221,14 @@ public class Instructions {
 				s.append("var").append(elem2).append("= var");
 				s.append(elem3).append(" or var").append(elem4);
 				break;
+				
 			case "Not":
 				elem2=quad.getElement2();
 				elem3=quad.getElement3();
 				elem4= quad.getElement4();
 				s.append("var").append(elem2).append("= not var").append(elem3);
 				break;
+				
 			default:
 				s.append(tab);
 				s.append("bouchon, op non implementé\n");
